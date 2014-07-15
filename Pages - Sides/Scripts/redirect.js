b950fefc9a6ba9522c32c7260b01c744fa45acf3
mahirk <mahirk@intelfinity.com>
2014-07-16 01:44:45 +0530

    Corrected Moz and opera error

--- Pages - Sides/Scripts/redirect.js
+++ Pages - Sides/Scripts/redirect.js
@@ -1,16 +1,16 @@
 var items = new Array();
-items["autix"] = "http://flc.co.in";
+items["autix"] = "https://github.com/mahirk/autixdev";
 items["flc"] = "http://flc.co.in";
-items["rise"] = "http://flc.co.in";
-items["pathify"] = "http://flc.co.in";
-items["catlyft"] = "http://flc.co.in";
-items["c4k"] = "http://flc.co.in";
-items["chowdown"] = "http://flc.co.in";
-items["suw"] = "http://flc.co.in";
+items["rise"] = "../Files/Extended Essay_Virtulaization_Mahir_signed.pdf";
+items["pathify"] = "http://pathify.info";
+items["catlyft"] = "https://github.com/mahirk/catlyft";
+items["c4k"] = "https://github.com/mahirk/CodingForKids";
+items["chowdown"] = "https://github.com/mahirk/chowdown";
+items["suw"] = "http://startupuw.com";
 items["otg"] = "http://flc.co.in";
-items["amun"] = "http://flc.co.in";
-items["aaa"] = "http://flc.co.in";
-items["interaa"] = "http://flc.co.in";
+items["amun"] = "http://abwamun.org";
+items["aaa"] = "http://abwaalumni.com";
+items["interaa"] = "http://interaa.in";
 
 $(window).load(function() {
    $("#projlist").bind('click', $.proxy(function (event) {
